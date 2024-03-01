ae3f5c2d6af2ae0bab84029ad727dda84a99c8be
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-03-01 14:44:13 -0300

    ok

---
+++ nelci/script.js
@@ -0,0 +1,10 @@
+fetch('https://my-json-server.typicode.com/professornelcimariano/lista_js/db')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Erro ao buscar os Dados:', error))
+
+
+ function manipulaHtml() {
+    
+    // alert('o documento será alterado')
+}
