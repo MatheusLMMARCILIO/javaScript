f442b1fcc9c33119d1599ff8b7cd95236b5a2241
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-03-01 16:27:10 -0300

    ok

--- nelci/script.js
+++ nelci/script.js
@@ -1,10 +1,28 @@
-fetch('https://my-json-server.typicode.com/professornelcimariano/lista_js/db')
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Erro ao buscar os Dados:', error))
+// fetch('https://my-json-server.typicode.com/professornelcimariano/lista_js/db')
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => console.error('Erro ao buscar os Dados:', error))
+
+let x = 0
+let mudaTexto = document.getElementById('listaVideos')
+let contadorexibe = document.getElementById("contaor")
+let contador = 0
 
 
- function manipulaHtml() {
-    
-    // alert('o documento será alterado')
+function manipulaHtml() {
+
+    if (x == 0) {
+        mudaTexto.innerHTML = 'Óla, mnupulamos os dados'
+        x = 1
+        contador = contador + 1
+        contadorexibe.innerHTML = contador
+
+    } else {
+        mudaTexto.innerHTML = 'dados'
+        x = 0
+        contador = contador - 1
+        contadorexibe.innerHTML = contador
+    }
+
 }
+
