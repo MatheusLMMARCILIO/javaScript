8db18fc3c08c0e05f915f1b2f7b2150b4b4194fb
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-03-08 15:04:30 -0300

    ok

---
+++ makeLogin/script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('.Formulario');
+
+
+form.addEventListener("submit", function (event) {
+    const login = document.getElementById('Login').value
+    const password = document.getElementById('senha').value
+    alert(login+' '+senha)
+
+} )
